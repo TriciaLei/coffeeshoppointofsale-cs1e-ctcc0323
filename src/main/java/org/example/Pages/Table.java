5b8e51ae968f4c586ce6721cc60b614620beddda
Tricia Lei Alburo <165367678+TriciaLei@users.noreply.github.com>
2024-06-15 11:37:20 +0800

    Revert "hhehehe add"

--- src/main/java/org/example/Pages/Table.java
+++ src/main/java/org/example/Pages/Table.java
@@ -6,15 +6,10 @@ import javax.swing.*;
 import java.awt.*;
 public class Table extends CoffeePanel {
     public Table() {
-        CoffeeImage image1 = new CoffeeImage(new ImageIcon("/Users/trixiemaeb.alburo/Documents/GitHub/coffeeshoppointofsale-cs1e-ctcc0323/src/table1.png"));
+        CoffeeImage image1 = new CoffeeImage(new ImageIcon("/Users/trixiemaeb.alburo/Documents/GitHub/coffeeshoppointofsale-cs1e-ctcc0323/src/kopiii.png"));
        add(image1);
        image1.setLocation(200,15);
-       image1.setSize(128,128);
-        CoffeeImage image2 = new CoffeeImage(new ImageIcon("/Users/trixiemaeb.alburo/Documents/GitHub/coffeeshoppointofsale-cs1e-ctcc0323/src/table1.png"));
-        add(image2);
-
-        image2.setLocation(350,15);
-        image2.setSize(128,128);
+       image1.setSize(50,50);
 
 
     }
