d09321d899a0cf5ec5249aeea295176281286cb7
Ruzzel Mendoza <ruzzelforpersonal@gmail.com>
2024-06-15 15:57:14 +0800

    resolved

--- src/main/java/org/example/MenuItemData.java
+++ src/main/java/org/example/MenuItemData.java
@@ -9,13 +9,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MenuItemData {
-
+    
     public HashMap<ItemData, String> items = new HashMap<>();
-
+    
     public ArrayList<String> types = new ArrayList<>();
-
-
-
+    
+    
     public MenuItemData() {
         readTypes();
         readItems();
@@ -27,25 +26,25 @@ public class MenuItemData {
 //
 //        System.out.println("");
     }
-
+    
     // public ArrayList<ItemData> getItems(){
     //     ArrayList<ItemData> tmp = new ArrayList<>();
-
+    
     //     for (Map.Entry<ItemData, String> set : items.entrySet()){
-
+    
     //         tmp.add(set.getKey());
     //     }
-
-
+    
+    
     //     return tmp;
-
+    
     // }
-
+    
     private void readItems() {
         for (String t : types) {
             try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/items/" + t + ".txt"))) {
-
-
+                
+                
                 String line = "";
                 int i = 0;
                 ItemData data = new ItemData();
@@ -79,82 +78,86 @@ public class MenuItemData {
                             items.put(data, t);
                             i = 0;
                             break;
-
+                        
                     }
                 }
-
-            }catch (IOException e){
-
+                
+            } catch (IOException e) {
+            
             }
         }
     }
-
+    
     private void readTypes() {
         try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/MenuTypes.txt"))) {
-        getMenuTypes();
-        getItems();
-
-        for(Map.Entry<ItemData, String> set : items.entrySet()){
-            System.out.println(set.getKey().name + set.getKey().price);
+            getMenuTypes();
+            getItems();
+            
+            for (Map.Entry<ItemData, String> set : items.entrySet()) {
+                System.out.println(set.getKey().name + set.getKey().price);
+            }
+        } catch (IOException e) {
+	        throw new RuntimeException(e);
         }
+	    
     }
-
-    private void getItems() {
-        for (String t : types) {
-            try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/items/" + t + ".txt"))) {
-
-
+        
+        private void getItems() {
+            for (String t : types) {
+                try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/items/" + t + ".txt"))) {
+                    
+                    
+                    String line = "";
+                    int i = 0;
+                    ItemData data = new ItemData();
+                    while ((line = reader.readLine()) != null) {
+                        switch (i) {
+                            case 0:
+                                data.name = line;
+                                i++;
+                                break;
+                            case 1, 3, 4, 5:
+                                i++;
+                                break;
+                            case 2:
+                                data.price = line;
+                                i++;
+                                break;
+                            default:
+                                items.put(data, t);
+                                i = 0;
+                                break;
+                            
+                        }
+                    }
+                } catch (IOException e) {
+                
+                }
+            }
+        }
+        
+        private void getMenuTypes() {
+            try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/MenuTypes.txt"))) {
+                
                 String line = "";
+                
                 int i = 0;
-                ItemData data = new ItemData();
                 while ((line = reader.readLine()) != null) {
                     switch (i) {
                         case 0:
-                            data.name = line;
+                            types.add(line);
                             i++;
                             break;
-                        case 1, 3, 4, 5:
-                            i++;
-                            break;
-                        case 2:
-                            data.price = line;
+                        case 1, 2, 3, 4, 5:
                             i++;
                             break;
                         default:
-                            items.put(data, t);
                             i = 0;
                             break;
-
                     }
                 }
-            }catch (IOException e){
-
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
-    }
-
-    private void getMenuTypes() {
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/MenuTypes.txt"))) {
-
-            String line = "";
-
-            int i = 0;
-            while ((line = reader.readLine()) != null) {
-                switch (i) {
-                    case 0:
-                        types.add(line);
-                        i++;
-                        break;
-                    case 1, 2, 3, 4, 5:
-                        i++;
-                        break;
-                    default:
-                        i = 0;
-                        break;
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
